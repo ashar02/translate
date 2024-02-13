fee214088ddcd4dd68a1c5e49cbe7146099effcd
MAshar <ashar@claryicon.com>
2024-02-13 19:32:38 +0500

    Analytics

--- src/app/core/modules/google-analytics/google-analytics.service.ts
+++ src/app/core/modules/google-analytics/google-analytics.service.ts
@@ -1,11 +1,19 @@
 import {Injectable} from '@angular/core';
 import {getCLS, getFID, getLCP} from 'web-vitals';
-import {FirebaseAnalytics} from '@capacitor-firebase/analytics';
-import {FirebasePerformance} from '@capacitor-firebase/performance';
+//import {FirebaseAnalytics} from '@capacitor-firebase/analytics';
+//import {FirebasePerformance} from '@capacitor-firebase/performance';
 import {environment} from '../../../../environments/environment';
 
 function isPromise(promise) {
   return !!promise && typeof promise.then === 'function';
+}
+
+declare var gtag: Function;
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
 }
 
 @Injectable({
@@ -15,18 +23,37 @@ export class GoogleAnalyticsService {
   traces: {name: string; time: number}[] = [];
 
   constructor() {
+    this.initializeGoogleAnalytics();
     this.logPerformanceMetrics();
   }
 
   get isSupported() {
-    return environment.firebase.measurementId && 'window' in globalThis && 'document' in globalThis;
+    return environment.firebase.measurementId1 && 'window' in globalThis && 'document' in globalThis;
+  }
+
+  initializeGoogleAnalytics() {
+    if (environment.firebase.measurementId1) {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.firebase.measurementId1}`;
+      // Append the script element to the document head
+      document.head.appendChild(script);
+      // Initialize Google Analytics with the custom Measurement ID
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: any[]) {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', environment.firebase.measurementId1);
+    }
   }
 
   async setCurrentScreen(screenName: string) {
     if (!this.isSupported) {
       return;
     }
-    await FirebaseAnalytics.setCurrentScreen({screenName});
+    //await FirebaseAnalytics.setCurrentScreen({screenName});
+    gtag('config', environment.firebase.measurementId1, {'page_path': screenName});
   }
 
   logPerformanceMetrics() {
@@ -35,7 +62,7 @@ export class GoogleAnalyticsService {
     }
 
     const sendToGoogleAnalytics = ({name, delta, value, id}) => {
-      return FirebaseAnalytics.logEvent({
+      /*return FirebaseAnalytics.logEvent({
         name,
         params: {
           value: delta,
@@ -43,6 +70,11 @@ export class GoogleAnalyticsService {
           metric_value: value,
           metric_delta: delta,
         },
+      });*/
+      gtag('event', name, {
+        'event_category': 'Web Vitals',
+        'event_label': name,
+        'value': delta,
       });
     };
 
@@ -52,12 +84,12 @@ export class GoogleAnalyticsService {
   }
 
   async trace<T>(timingCategory: string, timingVar: string, callable: () => T): Promise<T> {
-    if (!this.isSupported) {
+    //if (!this.isSupported) {
       return callable();
-    }
+    //}
 
     const startTime = performance.now();
-    const traceName = `${timingCategory}:${timingVar}`;
+    /*const traceName = `${timingCategory}:${timingVar}`;
     await FirebasePerformance.startTrace({traceName});
     const stopTrace = () => {
       this.traces.push({name: traceName, time: performance.now() - startTime});
@@ -72,7 +104,23 @@ export class GoogleAnalyticsService {
       }) as any;
     } else {
       stopTrace();
+    }*/
+    const call = callable();
+    if (isPromise(call)) {
+      await (call as Promise<any>);
     }
+    const endTime = performance.now();
+    const duration = endTime - startTime;
+    this.traces.push({ name: `${timingCategory}:${timingVar}`, time: duration });
+
+    const sendToGoogleAnalytics = ({ name, delta }) => {
+      gtag('event', 'Web Vitals', {
+        'event_category': 'Performance',
+        'event_label': name,
+        'value': delta,
+      });
+    };
+    sendToGoogleAnalytics({name: `${timingCategory}:${timingVar}`, delta: duration});
 
     return call;
   }
